65e10682ffee95d491852470a783d193a966d7ed
Hituzi Ando <hhh.ando@gmail.com>
2017-03-26 01:10:48 +0900

    first commit.

---
+++ MKAToastView/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  MKAToastView
+//
+//  Created by Masaki Ando on 2017/03/25.
+//  Copyright © 2017年 Hituzi Ando. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "MKAToastView.h"
+
+@interface ViewController () <MKAToastViewDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showButtonClicked:(id)sender {
+    [MKAToastView showInView:self.view withMessage:@"Hello World!" delegate:self timeInterval:MKAToastViewShortTime
+                  identifier:1];
+}
+
+#pragma mark - MKAToastViewDelegate
+
+- (void)toastViewWillDisappear:(MKAToastView *)toastView {
+    NSLog(@"%s", __func__);
+}
+
+- (void)toastViewDidDisappear:(MKAToastView *)toastView {
+    if (toastView.identifier == 1) {
+        NSLog(@"%s", __func__);
+    }
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,56 @@
+MKAToastView
+===
+
+Android-like Toast view.
+
+# Usage
+
+```
+#import "MKAToastView.h"
+
+...
+
+// Show.
+[MKAToastView showInView:self.view withMessage:@"Hello World!" delegate:nil timeInterval:MKAToastViewShortTime];
+
+```
+
+### Delegate
+
+```
+@interface ViewController () <MKAToastViewDelegate>
+
+@end
+
+@implementation ViewController
+
+...
+
+- (IBAction)showButtonClicked:(id)sender {
+	// Show.
+	[MKAToastView showInView:self.view 
+	             withMessage:@"Hello World!" 
+	                delegate:self 
+	            timeInterval:MKAToastViewShortTime
+	              identifier:1];
+}
+
+...
+
+- (void)toastViewWillDisappear:(MKAToastView *)toastView {
+	if (toastView.identifier == 1) {
+		NSLog(@"%s", __func__);
+	}
+}
+
+- (void)toastViewDidDisappear:(MKAToastView *)toastView {
+	if (toastView.identifier == 1) {
+		NSLog(@"%s", __func__);
+	}
+}
+
+@end
+```
+
+# License
+MIT License
